b87c1d3193d94c15082559bfcc988b85d96bf4a8
katrina-demkowicz <kpd8178@g.rit.edu>
2026-05-25 23:45:29 -0400

    added method to process book data and make object

---
+++ book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self, book_ID, copy_ID, title, author_first, author_last, summary, subjects):
+        self.book_ID = book_ID
+        self.copy_ID = copy_ID
+        self.title = title
+        self.author_first = author_first
+        self.author_last = author_last
+        self.summary = summary
+        self.subjects = subjects
--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import time
+from book import Book
 
 def scrape():
     # for the first attempt, going to scrape the library's page on
     # Toni Morrison's Jazz
-    copy_ID = get_book_copy(142763)
+    book_ID = 142763
+    #book_ID = 855335
+    copy_ID = get_book_copy(book_ID)
     # make sure copy_ID isn't None or NULL
     copy_data = get_book_copy_data(copy_ID)
-    print(copy_data)
+    process_book(copy_data, book_ID, copy_ID)
 
 '''Get the first copy of the book that is in a book format'''
 def get_book_copy(book_ID):
@@ -39,8 +42,64 @@ def get_book_copy_data(copy_ID):
     except requests.exceptions.RequestException as error:
         print(f"Error occurred: {error}")
 
-def insert_book(copy_data):
-    pass
+'''Process book data and return a book object'''
+def process_book(copy_data, book_ID, copy_ID):
+    book_title = ''
+    book_author_first = ''
+    book_author_last = ''
+    book_summary = ''
+    book_subjects = ''
+
+    for item in copy_data:
+        label = item['label']
+        value = item['detailsValues']
+
+        #continue if value is empty
+        if not value:
+            continue
+
+        #get title
+        if label == 'Title':
+            full_book_title = value[0]['value']
+            book_title = full_book_title.split('/')[0].strip()
+            # print(book_title)
+
+        #get author
+        if label == 'Author':
+            author_full_name = value[0]['linkValue']
+            book_author_last = author_full_name.split(',')[0].strip()
+            book_author_first = author_full_name.split(',')[1].strip()
+            # print(book_author_last)
+            # print(book_author_first)
+
+        #get summary
+        if label == 'Summary':
+            book_summary = value[0]['value']
+            # print(book_summary)
+
+        #get subjects
+        if label == 'Subjects':
+            subjects = []
+            seen = set()
+
+            #go through each value and add it to a list
+            for v in value:
+                subject = v['value']
+                subject = subject.replace(' -- Fiction', '').strip()
+                subject = subject.lower()
+
+                #make sure there are no duplicates
+                if subject not in seen:
+                    subjects.append(subject)
+                    seen.add(subject)
+            
+            book_subjects = json.dumps(subjects)
+            # print(book_subjects)
+    
+    #make book object
+    book = Book(book_ID, copy_ID, book_title, book_author_first, 
+                book_author_last, book_summary, book_subjects)     
+    return book
 
 if __name__ == '__main__':
     scrape()
